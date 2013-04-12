32ec1bc9801035ac48a57e737637651153af7877
Jan Vlnas <git@jan.vlnas.cz>
2013-04-12 21:12:29 +0200

    Remove rspec on rails plugin

--- vendor/plugins/rspec-on-rails-matchers/init.rb
+++
@@ -1,8 +0,0 @@
-if Rails.env == "test"
-  # We are only loading the modules we are using right now
-  # require 'spec/rails/matchers/observers'
-  require 'rspec/rails/matchers/associations'
-  require 'rspec/rails/matchers/validations'
-  # require 'spec/rails/matchers/views'
-  # require 'spec/rails/matchers/observers'
-end
