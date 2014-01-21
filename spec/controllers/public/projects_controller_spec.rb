b6ea7aa856491095c8ae9a8b7c343c1a5b1eeb08
Cesar Carruitero <cesar@mozilla.pe>
2014-01-21 08:33:56 -0500

    fix public controller spec

--- spec/controllers/public/projects_controller_spec.rb
+++ spec/controllers/public/projects_controller_spec.rb
@@ -2,10 +2,10 @@
 require 'spec_helper'
 
 describe Public::ProjectsController do
+  render_views
 
   before do
-    @public_project = Factory(:project)
-    @public_project.public = true
+    @public_project = Factory(:project, public: true)
   end
 
   describe '#index' do
@@ -17,7 +17,8 @@ describe Public::ProjectsController do
 
   describe '#show' do
     it 'should show public project' do
-      get :show, id: @public_project.name
+      get :show, id: @public_project.permalink
+      response.should be_success
       response.should have_selector 'title', content: @public_project.name
     end
   end
