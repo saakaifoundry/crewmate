fbf83b41f43728ad9c1754b258b11c754106ac0d
Andrea Dal Ponte <info@andreadalponte.com>
2014-03-22 18:23:20 +0100

    Specs for Person.user_names_from_projects class method

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -2,7 +2,6 @@
 require 'spec_helper'
 
 describe Person do
-
   it "clears the assigned user on tasks when destroyed" do
     task = Factory :task
     person = task.project.people.first
@@ -37,5 +36,49 @@ describe Person do
     @project1.add_user(@user)
     @project2.add_user(@user)
     @user.projects(true).should include(@project1, @project2)
+  end
+
+  describe '.user_names_from_projects' do
+    let!(:project1) { Factory(:project) }
+    let!(:project2) { Factory(:project) }
+    let!(:user1)    { project1.user }
+    let!(:user2)    { project2.user }
+    let!(:user3)    { Factory(:user) }
+
+    before :each do
+      project1.add_user(user3)
+    end
+
+    subject(:user_names) { Person.user_names_from_projects(project1) }
+
+    its 'size should be 2' do
+      user_names.size.should eql 2
+    end
+
+    it 'should contain user1' do
+      user_names.map(&:user_id).should include(user1.id)
+    end
+
+    it 'should contain user3' do
+      user_names.map(&:user_id).should include(user3.id)
+    end
+
+    it 'should not contain user2' do
+      user_names.map(&:user_id).should_not include(user2.id)
+    end
+
+    it 'should contain project1' do
+      user_names.map(&:project_id).should include(project1.id)
+    end
+
+    it 'should not contain project2' do
+      user_names.map(&:project_id).should_not include(project2.id)
+    end
+
+    context 'his first result' do
+      subject(:user_name) { user_names.first }
+
+      it { should respond_to(:project_id, :login, :first_name, :last_name, :user_id) }
+    end
   end
 end
