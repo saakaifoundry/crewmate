a6a4e53b7b58505d81960306fb8e8c733ce36590
Andrea Dal Ponte <info@andreadalponte.com>
2014-07-25 14:27:05 +0200

    Use Timecop.travel instead Date or Time stub

--- features/step_definitions/time_steps.rb
+++ features/step_definitions/time_steps.rb
@@ -1,18 +1,17 @@
 # -*- encoding : utf-8 -*-
-require 'timecop'
 
 Given /^today is "([^\"]*)"$/ do |date|
-  Date.stub!(:today).and_return(Date.parse(date))
+  Timecop.travel Date.parse(date)
 end
 
 Given /^today is ([a-z]+)$/i do |day|
   target_wday = %w[sun mon tue wed thu fri sat].index(day[0, 3].downcase)
   now = Time.now
-  Time.stub!(:now).and_return(now.advance(:days => target_wday - now.wday))
+  Timecop.travel now.advance(:days => target_wday - now.wday)
 end
 
 Given /^the time is "([^\"]*)"$/ do |time|
-  Time.stub!(:now).and_return(Time.parse(time))
+  Timecop.travel(time)
 end
 
 Given /^utc time is now (\d+) hours? and a bit before midnight$/ do |hours|
